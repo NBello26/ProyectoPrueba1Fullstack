657ee44e80f32c551436525b479de415b628ab56
NBello26 <163012023+NBello26@users.noreply.github.com>
2025-09-08 13:35:11 -0300

    Codigo base registro-login
    Base del codigo para registro-login, formulario, guardar,css,js

---
+++ js/login.js
@@ -0,0 +1,25 @@
+document.getElementById("formLogin").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    let correoLogin = document.getElementById("correoLogin").value;
+    let contrasenaLogin = document.getElementById("contrasenaLogin").value;
+
+    // Obtener los usuarios almacenados en localStorage
+    let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+
+    // Buscar el usuario que coincida con el correo y la contraseña
+    let usuarioEncontrado = usuarios.find(usuario => usuario.correo === correoLogin && usuario.contrasena === contrasenaLogin);
+
+    if (usuarioEncontrado) {
+        // Redirigir según el tipo de usuario
+        if (usuarioEncontrado.tipusuario === "cliente") {
+            window.location.href = "pagina_principal.html"; // Redirige a la página principal
+        } else if (usuarioEncontrado.tipusuario === "vendedor") {
+            window.location.href = "pagvendedor.html"; // Redirige a la página de vendedor
+        } else if (usuarioEncontrado.tipusuario === "admin") {
+            window.location.href = "pagadmin.html"; // Redirige a la página de admin
+        }
+    } else {
+        alert("Correo o contraseña incorrectos");
+    }
+});
